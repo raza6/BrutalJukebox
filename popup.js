309856e251312625ad3fc724b9e2b504b2137fa8
Bobby <antonin.gontier@insa-rennes.fr>
2020-08-14 23:22:05 +0200

    Update popup depending on extension state

--- popup.js
+++ popup.js
@@ -2,5 +2,15 @@ window.onload = function() {
     const extensionState = document.getElementById('extensionState');
     extensionState.addEventListener('change', function() {
         chrome.storage.local.set({'brutalJukeboxState': extensionState.checked});
+        const wrap = document.getElementById('extensionStateWrapper');
+        const title = wrap.querySelector('h2');
+        const label = wrap.querySelector('#extensionStateWrapper > span');
+        if (extensionState.checked) {
+            title.innerText = 'Disable extension'
+            label.innerText = 'Enabled 🤩'
+        } else {
+            title.innerText = 'Enable extension'
+            label.innerText = 'Disabled 😴'
+        }
     });
 }
