51fa04a7bb93df3dce5b2a032fd6c9fb0c56de77
Antonin Gontier <gontier.antonin@orange.fr>
2021-11-06 01:03:49 +0100

    Remove options

--- extension/options.js
+++ extension/options.js
@@ -1,8 +1,11 @@
 /* eslint-disable no-undef */
 window.onload = () => {
+  /* NOT USED ---- API KEY PASSED BY JSON FILE */
+  /*
   const ipt = document.getElementById('apikey');
   ipt.addEventListener('blur', () => {
     // eslint-disable-next-line quote-props
     chrome.storage.local.set({ 'twitterAPIKey': ipt.value });
   });
+  */
 };
